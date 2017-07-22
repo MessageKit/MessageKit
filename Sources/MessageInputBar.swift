7c4eed80ff97bef4b1115b32c3aeef05a68fcd7a
Steven Deutsch <stevensdeutsch@yahoo.com>
2017-07-22 16:14:54 -0500

    Setup basic MessageInputBar with constraints

--- Sources/MessageInputBar.swift
+++ Sources/MessageInputBar.swift
@@ -25,9 +25,32 @@
 import UIKit
 
 open class MessageInputBar: UIView {
+    
+    // MARK: - Properties
+    
+    let inputTextView: UITextView = {
+    
+        let inputTextView = UITextView(frame: .zero)
+        inputTextView.text = "Some example text for testing"
+        inputTextView.backgroundColor = .white
+        return inputTextView
+    }()
+    
+    let sendButton: UIButton = {
+    
+        let sendButton = UIButton()
+        sendButton.setTitle("Send", for: .normal)
+        sendButton.setTitleColor(.lightGray, for: .normal)
+        sendButton.backgroundColor = .red
+        return sendButton
+    }()
+    
+    // MARK: - Initializers
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupSubviews()
+        setupConstraints()
     }
     
     convenience init() {
@@ -36,6 +59,30 @@ open class MessageInputBar: UIView {
     
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+    
+    func setupSubviews() {
+        
+        addSubview(inputTextView)
+        addSubview(sendButton)
+        
+    }
+    
+    func setupConstraints() {
+
+        inputTextView.translatesAutoresizingMaskIntoConstraints = false
+        addConstraint(NSLayoutConstraint(item: inputTextView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 4))
+        addConstraint(NSLayoutConstraint(item: inputTextView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: -4))
+        addConstraint(NSLayoutConstraint(item: inputTextView, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1, constant: 4))
+        addConstraint(NSLayoutConstraint(item: inputTextView, attribute: .trailing, relatedBy: .equal, toItem: sendButton, attribute: .leading, multiplier: 1, constant: -4))
+
+        sendButton.translatesAutoresizingMaskIntoConstraints = false
+        addConstraint(NSLayoutConstraint(item: sendButton, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: sendButton, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: sendButton, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0))
+
     }
 
 
