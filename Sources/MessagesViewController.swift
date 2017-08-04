d01aac40a5ad1d9bcb91205bbb8947a043d5822d
MacmeDan <pdanielleonard@gmail.com>
2017-08-04 16:46:26 -0600

    Removed unnecessary Swift lint rules. Fixed all other warnings. `Avatar` class along with `Avatar.playground` added with documentation for people to play with the class. Tests added.

--- Sources/MessagesViewController.swift
+++ Sources/MessagesViewController.swift
@@ -1,123 +1,123 @@
 /*
-MIT License
-
-Copyright (c) 2017 MessageKit
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+ MIT License
+ 
+ Copyright (c) 2017 MessageKit
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
 
 import UIKit
 
 open class MessagesViewController: UIViewController {
-
-	// MARK: - Properties
-
-	open var messagesCollectionView = MessagesCollectionView(frame: .zero, collectionViewLayout: MessagesCollectionViewFlowLayout())
-
-	open var messageInputBar = MessageInputBar()
-
-	override open var canBecomeFirstResponder: Bool {
-		return true
-	}
-
-	override open var inputAccessoryView: UIView? {
-		return messageInputBar
-	}
-
+    
+    // MARK: - Properties
+    
+    open var messagesCollectionView = MessagesCollectionView(frame: .zero, collectionViewLayout: MessagesCollectionViewFlowLayout())
+    
+    open var messageInputBar = MessageInputBar()
+    
+    override open var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    override open var inputAccessoryView: UIView? {
+        return messageInputBar
+    }
+    
     open override var shouldAutorotate: Bool {
         return false
     }
-
-	// MARK: - View Life Cycle
-
-	open override func viewDidLoad() {
-		super.viewDidLoad()
-
-		automaticallyAdjustsScrollViewInsets = false
-
-		setupSubviews()
-		setupConstraints()
-		registerReusableViews()
-		setupDelegates()
-
-	}
-
+    
+    // MARK: - View Life Cycle
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        automaticallyAdjustsScrollViewInsets = false
+        
+        setupSubviews()
+        setupConstraints()
+        registerReusableViews()
+        setupDelegates()
+        
+    }
+    
     open override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         messagesCollectionView.scrollToBottom(animated: true)
     }
-
-	open override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(animated)
-		// depends on inputAccessoryView frame thus must be called here
-		addKeyboardObservers()
+    
+    open override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // depends on inputAccessoryView frame thus must be called here
+        addKeyboardObservers()
         messagesCollectionView.scrollToBottom(animated: false)
-	}
-
-	open override func viewWillDisappear(_ animated: Bool) {
-		super.viewWillDisappear(animated)
-		removeKeyboardObservers()
-	}
-
-	// MARK: - Methods
-
-	private func setupDelegates() {
-		messagesCollectionView.delegate = self
-		messagesCollectionView.dataSource = self
-	}
-
-	private func registerReusableViews() {
-
-		messagesCollectionView.register(MessageCollectionViewCell.self, forCellWithReuseIdentifier: "MessageCell")
-
+    }
+    
+    open override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        removeKeyboardObservers()
+    }
+    
+    // MARK: - Methods
+    
+    private func setupDelegates() {
+        messagesCollectionView.delegate = self
+        messagesCollectionView.dataSource = self
+    }
+    
+    private func registerReusableViews() {
+        
+        messagesCollectionView.register(MessageCollectionViewCell.self, forCellWithReuseIdentifier: "MessageCell")
+        
         messagesCollectionView.register(MessageHeaderView.self,
                                         forSupplementaryViewOfKind: UICollectionElementKindSectionHeader,
                                         withReuseIdentifier: "MessageHeader")
-
+        
         messagesCollectionView.register(MessageFooterView.self,
                                         forSupplementaryViewOfKind: UICollectionElementKindSectionFooter,
                                         withReuseIdentifier: "MessageFooter")
-	}
-
-	private func setupSubviews() {
-		messagesCollectionView.keyboardDismissMode = .interactive
-		view.addSubview(messagesCollectionView)
-	}
-
-	private func setupConstraints() {
-
-		messagesCollectionView.translatesAutoresizingMaskIntoConstraints = false
-
-		view.addConstraint(NSLayoutConstraint(item: messagesCollectionView, attribute: .top, relatedBy: .equal,
-		                                      toItem: topLayoutGuide, attribute: .bottom, multiplier: 1, constant: 0))
-
-		view.addConstraint(NSLayoutConstraint(item: messagesCollectionView, attribute: .leading, relatedBy: .equal,
-		                                      toItem: view, attribute: .leading, multiplier: 1, constant: 0))
-
-		view.addConstraint(NSLayoutConstraint(item: messagesCollectionView, attribute: .trailing, relatedBy: .equal,
-		                                      toItem: view, attribute: .trailing, multiplier: 1, constant: 0))
-
-		view.addConstraint(NSLayoutConstraint(item: messagesCollectionView, attribute: .bottom, relatedBy: .equal,
-		                                      toItem: bottomLayoutGuide, attribute: .top, multiplier: 1, constant: -48))
-
-	}
-
+    }
+    
+    private func setupSubviews() {
+        messagesCollectionView.keyboardDismissMode = .interactive
+        view.addSubview(messagesCollectionView)
+    }
+    
+    private func setupConstraints() {
+        
+        messagesCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addConstraint(NSLayoutConstraint(item: messagesCollectionView, attribute: .top, relatedBy: .equal,
+                                              toItem: topLayoutGuide, attribute: .bottom, multiplier: 1, constant: 0))
+        
+        view.addConstraint(NSLayoutConstraint(item: messagesCollectionView, attribute: .leading, relatedBy: .equal,
+                                              toItem: view, attribute: .leading, multiplier: 1, constant: 0))
+        
+        view.addConstraint(NSLayoutConstraint(item: messagesCollectionView, attribute: .trailing, relatedBy: .equal,
+                                              toItem: view, attribute: .trailing, multiplier: 1, constant: 0))
+        
+        view.addConstraint(NSLayoutConstraint(item: messagesCollectionView, attribute: .bottom, relatedBy: .equal,
+                                              toItem: bottomLayoutGuide, attribute: .top, multiplier: 1, constant: -48))
+        
+    }
+    
 }
 
 // MARK: - UICollectionViewDelegate & UICollectionViewDelegateFlowLayout Conformance
@@ -125,12 +125,12 @@ open class MessagesViewController: UIViewController {
 //swiftlint:disable line_length
 
 extension MessagesViewController: UICollectionViewDelegateFlowLayout {
-
-	public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-		guard let messagesFlowLayout = collectionViewLayout as? MessagesCollectionViewFlowLayout else { return .zero }
-		return messagesFlowLayout.sizeForItem(at: indexPath)
-	}
-
+    
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        guard let messagesFlowLayout = collectionViewLayout as? MessagesCollectionViewFlowLayout else { return .zero }
+        return messagesFlowLayout.sizeForItem(at: indexPath)
+    }
+    
 }
 
 //swiftlint:enable line_length
@@ -138,28 +138,28 @@ extension MessagesViewController: UICollectionViewDelegateFlowLayout {
 // MARK: - UICollectionViewDataSource Conformance
 
 extension MessagesViewController: UICollectionViewDataSource {
-
-	public func numberOfSections(in collectionView: UICollectionView) -> Int {
-		guard let collectionView = collectionView as? MessagesCollectionView else { return 0 }
-
-		// Each message is its own section
-		return collectionView.messagesDataSource?.numberOfMessages(in: collectionView) ?? 0
-	}
-
-	public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		guard let collectionView = collectionView as? MessagesCollectionView else { return 0 }
-
-		let messageCount = collectionView.messagesDataSource?.numberOfMessages(in: collectionView) ?? 0
-		// There will only ever be 1 message per section
-		return messageCount > 0 ? 1 : 0
-
-	}
+    
+    public func numberOfSections(in collectionView: UICollectionView) -> Int {
+        guard let collectionView = collectionView as? MessagesCollectionView else { return 0 }
+        
+        // Each message is its own section
+        return collectionView.messagesDataSource?.numberOfMessages(in: collectionView) ?? 0
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        guard let collectionView = collectionView as? MessagesCollectionView else { return 0 }
+        
+        let messageCount = collectionView.messagesDataSource?.numberOfMessages(in: collectionView) ?? 0
+        // There will only ever be 1 message per section
+        return messageCount > 0 ? 1 : 0
+        
+    }
 
     //swiftlint:disable line_length
 
-	public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
-		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MessageCell", for: indexPath) as? MessageCollectionViewCell ?? MessageCollectionViewCell()
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MessageCell", for: indexPath) as? MessageCollectionViewCell ?? MessageCollectionViewCell()
 
         guard let messagesCollectionView = collectionView as? MessagesCollectionView else { return cell }
         guard let messageCellDelegate = messagesCollectionView.messageCellDelegate else { return cell }
@@ -173,15 +173,16 @@ extension MessagesViewController: UICollectionViewDataSource {
         let messageColor = displayDataSource.messageColorFor(message, at: indexPath, in: messagesCollectionView)
         let avatar = displayDataSource.avatarForMessage(message, at: indexPath, in: messagesCollectionView)
 
-        cell.avatarImageView.image = avatar.image(highlighted: false)
-        cell.avatarImageView.highlightedImage = avatar.image(highlighted: true)
+        //TODO: Replace avatarImageView with an Avatar class
+        cell.avatarImageView.image =  avatar.getImage()
         cell.messageContainerView.backgroundColor = messageColor
         cell.configure(with: message)
 
-		return cell
+        return cell
 
-	}
-
+    }
+    
+    
     public func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
 
         guard let messagesCollectionView = collectionView as? MessagesCollectionView else { return UICollectionReusableView() }
@@ -197,14 +198,14 @@ extension MessagesViewController: UICollectionViewDataSource {
         default:
             fatalError("Unrecognized element of kind: \(kind)")
         }
-
+        
     }
 
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         guard let messagesCollectionView = collectionView as? MessagesCollectionView else { return .zero }
         guard let messagesDataSource = messagesCollectionView.messagesDataSource else { return .zero }
         guard let messagesLayoutDelegate = messagesCollectionView.messagesLayoutDelegate else { return .zero }
-         // Could pose a problem if subclass behaviors allows more than one item per section
+        // Could pose a problem if subclass behaviors allows more than one item per section
         let indexPath = IndexPath(item: 0, section: section)
         let message = messagesDataSource.messageForItem(at: indexPath, in: messagesCollectionView)
         return messagesLayoutDelegate.headerSizeFor(message, at: indexPath, in: messagesCollectionView)
@@ -220,42 +221,42 @@ extension MessagesViewController: UICollectionViewDataSource {
         return messagesLayoutDelegate.footerSizeFor(message, at: indexPath, in: messagesCollectionView)
     }
 
-    //swiftlint:enable line_length
-
 }
+
+
 
 // MARK: - Keyboard Handling
 extension MessagesViewController {
-
-	fileprivate func addKeyboardObservers() {
-
-		NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardWillHide), name: .UIKeyboardWillHide, object: nil)
-		NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardWillChangeFrame), name: .UIKeyboardWillChangeFrame, object: nil)
-
+    
+    fileprivate func addKeyboardObservers() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardWillHide), name: .UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardWillChangeFrame), name: .UIKeyboardWillChangeFrame, object: nil)
+        
     }
 
     fileprivate func removeKeyboardObservers() {
-
+        
         NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
         NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillChangeFrame, object: nil)
-
+        
     }
-
-	func handleKeyboardWillHide(_ notification: Notification) {
-
-		messagesCollectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-
-	}
-
-	func handleKeyboardWillChangeFrame(_ notification: Notification) {
-
-		guard let keyboardSizeValue = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue else { return }
-
-		let keyboardRect = keyboardSizeValue.cgRectValue
-		let messageInputBarHeight = inputAccessoryView?.bounds.size.height ?? 0
-		let keyboardHeight = keyboardRect.height - messageInputBarHeight
-		messagesCollectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight, right: 0)
-
-	}
-
+    
+    func handleKeyboardWillHide(_ notification: Notification) {
+        
+        messagesCollectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+    }
+    
+    func handleKeyboardWillChangeFrame(_ notification: Notification) {
+        
+        guard let keyboardSizeValue = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardRect = keyboardSizeValue.cgRectValue
+        let messageInputBarHeight = inputAccessoryView?.bounds.size.height ?? 0
+        let keyboardHeight = keyboardRect.height - messageInputBarHeight
+        messagesCollectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight, right: 0)
+        
+    }
+    
 }
