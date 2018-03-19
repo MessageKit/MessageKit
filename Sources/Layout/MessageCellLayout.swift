212473c26fc146cc954a9e3b52797aea053227b9
Steven Deutsch <stevensdeutsch@yahoo.com>
2018-03-18 22:23:25 -0500

    Create MessageCellLayout objects

---
+++ Sources/Layout/MessageCellLayout.swift
@@ -0,0 +1,85 @@
+/*
+ MIT License
+
+ Copyright (c) 2017-2018 MessageKit
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+import UIKit
+
+protocol MessageCellLayout {
+
+    var incomingAvatarSize: CGSize { get set }
+    var outgoingAvatarSize: CGSize { get set }
+
+    var incomingAvatarPosition: AvatarPosition { get set }
+    var outgoingAvatarPosition: AvatarPosition { get set }
+
+    var incomingMessagePadding: UIEdgeInsets { get set }
+    var outgoingMessagePadding: UIEdgeInsets { get set }
+
+    var incomingCellTopLabelAlignment: LabelAlignment { get set }
+    var outgoingCellTopLabelAlignment: LabelAlignment { get set }
+
+    var incomingCellBottomLabelAlignment: LabelAlignment { get set }
+    var outgoingCellBottomLabelAlignment: LabelAlignment { get set }
+
+}
+
+public struct TextMessageLayout: MessageCellLayout {
+
+    var incomingAvatarSize = CGSize(width: 30, height: 30)
+    var outgoingAvatarSize = CGSize(width: 30, height: 30)
+
+    var incomingAvatarPosition = AvatarPosition(vertical: .messageBottom)
+    var outgoingAvatarPosition = AvatarPosition(vertical: .messageBottom)
+
+    var incomingMessageLabelInsets = UIEdgeInsets(top: 7, left: 18, bottom: 7, right: 14)
+    var outgoingMessageLabelInsets = UIEdgeInsets(top: 7, left: 14, bottom: 7, right: 18)
+
+    var incomingMessagePadding = UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 30)
+    var outgoingMessagePadding = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 4)
+
+    var incomingCellTopLabelAlignment = LabelAlignment.messageLeading(.zero)
+    var outgoingCellTopLabelAlignment = LabelAlignment.messageTrailing(.zero)
+
+    var incomingCellBottomLabelAlignment = LabelAlignment.messageTrailing(.zero)
+    var outgoingCellBottomLabelAlignment = LabelAlignment.messageLeading(.zero)
+
+}
+
+public struct MediaMessageLayout: MessageCellLayout {
+
+    var incomingAvatarSize = CGSize(width: 30, height: 30)
+    var outgoingAvatarSize = CGSize(width: 30, height: 30)
+
+    var incomingAvatarPosition = AvatarPosition(vertical: .messageBottom)
+    var outgoingAvatarPosition = AvatarPosition(vertical: .messageBottom)
+
+    var incomingMessagePadding = UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 30)
+    var outgoingMessagePadding = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 4)
+
+    var incomingCellTopLabelAlignment = LabelAlignment.messageLeading(.zero)
+    var outgoingCellTopLabelAlignment = LabelAlignment.messageTrailing(.zero)
+
+    var incomingCellBottomLabelAlignment = LabelAlignment.messageTrailing(.zero)
+    var outgoingCellBottomLabelAlignment = LabelAlignment.messageLeading(.zero)
+
+}
