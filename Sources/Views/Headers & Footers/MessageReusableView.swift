a009e72be61fefbb4bdc8394516571adcc3d0791
Maxim Tsvetkov <777.maxxx@gmail.com>
2018-08-31 00:22:59 +0200

    Add init with coder for MessageReusableView

--- Sources/Views/Headers & Footers/MessageReusableView.swift
+++ Sources/Views/Headers & Footers/MessageReusableView.swift
@@ -33,7 +33,7 @@ open class MessageReusableView: UICollectionReusableView {
     }
 
     public required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
 
 }
