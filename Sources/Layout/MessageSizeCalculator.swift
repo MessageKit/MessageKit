c9c8f356c416d29dea71030131e4aebfb0f706b7
subdiox <subdiox@gmail.com>
2019-02-19 18:35:50 +0900

    fix duplicate modifier

--- Sources/Layout/MessageSizeCalculator.swift
+++ Sources/Layout/MessageSizeCalculator.swift
@@ -215,7 +215,7 @@ open class MessageSizeCalculator: CellSizeCalculator {
         return isFromCurrentSender ? outgoingAccessoryViewPadding : incomingAccessoryViewPadding
     }
     
-    open public func accessoryViewPosition(for message: MessageType) -> AccessoryPosition {
+    public func accessoryViewPosition(for message: MessageType) -> AccessoryPosition {
         let dataSource = messagesLayout.messagesDataSource
         let isFromCurrentSender = dataSource.isFromCurrentSender(message: message)
         return isFromCurrentSender ? outgoingAccessoryViewPosition : incomingAccessoryViewPosition
