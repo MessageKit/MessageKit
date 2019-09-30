1010eeb0d338cc2b16e177aa09a28a800913fe98
Vlada Radchenko <vlada.radchenko@nixsolutions.com>
2019-09-30 14:43:41 +0300

    add model

---
+++ Sources/Models/MessageStatus.swift
@@ -0,0 +1,15 @@
+//
+//  MessageStatus.swift
+//  MessageKit
+//
+//  Created by Vlada Radchenko on 7/24/19.
+//  Copyright © 2019 MessageKit. All rights reserved.
+//
+
+import Foundation
+
+public enum MessageStatus: String, Codable {
+
+    case delivered
+    case read
+}
