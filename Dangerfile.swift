7a185d62d721da0c6b537ae24134cc7ee247d755
Jakub Kaspar <kaspikk@gmail.com>
2020-03-10 11:34:05 +0100

    Run all tests on github actions, use swiftlint from example project pods

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -19,4 +19,4 @@ if additions + deletions > bigPRThreshold {
   warn("Pull Request size seems relatively large - Please consider splitting up your changes into smaller Pull Requests.")
 }
 
-SwiftLint.lint(inline: true)
+SwiftLint.lint(inline: true, swiftlintPath: "Example/Pods/SwiftLint/swiftlint")
