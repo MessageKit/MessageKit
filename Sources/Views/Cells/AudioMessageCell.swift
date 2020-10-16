04e1ebd7c04dcd9608d7f72a22b562601155ba2d
David Vittori <me@vittoridavid.com>
2020-10-16 10:33:44 +0200

    calling displaydelegate at the end of configure audico cell

--- Sources/Views/Cells/AudioMessageCell.swift
+++ Sources/Views/Cells/AudioMessageCell.swift
@@ -131,11 +131,11 @@ open class AudioMessageCell: MessageContentCell {
         playButton.imageView?.tintColor = tintColor
         durationLabel.textColor = tintColor
         progressView.tintColor = tintColor
-
-        displayDelegate.configureAudioCell(self, message: message)
-
+        
         if case let .audio(audioItem) = message.kind {
             durationLabel.text = displayDelegate.audioProgressTextFormat(audioItem.duration, for: self, in: messagesCollectionView)
         }
+
+        displayDelegate.configureAudioCell(self, message: message)
     }
 }
