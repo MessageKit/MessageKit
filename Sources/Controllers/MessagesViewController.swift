d0a89b345dd67f0a22268fd52ca02f34f0642c95
Mehmet Baykar <mbaykar@userlytics.com>
2021-11-18 10:59:03 +0100

    Add overridable UIScrollView Delegate

--- Sources/Controllers/MessagesViewController.swift
+++ Sources/Controllers/MessagesViewController.swift
@@ -526,3 +526,26 @@ UICollectionViewDelegateFlowLayout, UICollectionViewDataSource, UIGestureRecogni
         return abs(velocity.x) > abs(velocity.y)
     }
 }
+
+        // MARK: - UIScrollViewDelegate
+
+extension MessagesViewController:UIScrollViewDelegate{
+    
+    open func scrollViewDidScroll(_ scrollView: UIScrollView) { }
+    
+    open func scrollViewWillBeginDragging(_ scrollView: UIScrollView){ }
+    
+    open func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>){ }
+    
+    open func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool){ }
+    
+    open func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView){ }
+    
+    open func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) { }
+    
+    open func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) { }
+    
+    open func scrollViewDidScrollToTop(_ scrollView: UIScrollView){ }
+    
+    open func scrollViewDidChangeAdjustedContentInset(_ scrollView: UIScrollView) { }
+}
