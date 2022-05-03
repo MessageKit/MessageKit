2bd234b1e878f392089f166d6868ce644d6c9e95
martinpucik <martin.pucik@me.com>
2022-05-03 18:21:25 +0200

    chore: Dropped iOS 12 support

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ import PackageDescription
 
 let package = Package(
     name: "MessageKit",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(name: "MessageKit", targets: ["MessageKit"]),
     ],
