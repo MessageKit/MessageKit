ca44c95e57df84b714454405c2f91e0eee1f8cf7
乐升平 <shengping_le@intsig.net>
2025-03-29 18:14:36 +0800

    Fix the issue where scrollToLastItem does not take sectionInset.bottom and footerView into account.

--- Sources/Views/MessagesCollectionView.swift
+++ Sources/Views/MessagesCollectionView.swift
@@ -80,7 +80,7 @@ open class MessagesCollectionView: UICollectionView {
   // MARK: Public
 
   // NOTE: It's possible for small content size this wouldn't work - https://github.com/MessageKit/MessageKit/issues/725
-  public func scrollToLastItem(at pos: UICollectionView.ScrollPosition = .bottom, animated: Bool = true) {
+  public func scrollToLastItem(at pos: UICollectionView.ScrollPosition = .top, animated: Bool = true) {
     guard let indexPath = indexPathForLastItem else { return }
 
     scrollToItem(at: indexPath, at: pos, animated: animated)
